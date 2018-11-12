cec704b71541579b5b889b7cfdc9afac410ab5d0
simplexVerborum <roberto@tummelier.com>
2018-11-12 19:30:42 -0400

    CU in progress

--- CU.v
+++ CU.v
@@ -1,100 +1,77 @@
-module control( input[5:0] opcode,  
-                           input reset,  
-                           output reg[1:0] reg_dst,mem_to_reg,
-               			   output reg[2:0] alu_op,
-                           output reg MOV, MOC, MAREnable, MDREanble, jump,branch,RW,alu_src,reg_write                     
+module control( 
+  input[5:0] opcode,  
+  input reset,  
+  output reg reg_dst,mem_to_reg,
+  output reg[2:0] alu_op,
+  output reg MOV, MOC, MAREnable, MDREanble, jump, branch, RW, alu_src, reg_write                     
    );  
+
  always @(*)  
  begin  
-      if(reset == 1'b1) begin  
-                reg_dst = 2'b00;  
-                mem_to_reg = 2'b00;  
+      if(reset == 1'b1) begin // noOp 
+                reg_dst = 1;  
+                mem_to_reg = 0;  
                 alu_op = 2'b00;  
-                jump = 1'b0;  
-                branch = 1'b0;  
-                mem_read = 1'b0;  
-                mem_write = 1'b0;  
-                alu_src = 1'b0;  
-                reg_write = 1'b0;  
+                jump = 0;  
+                branch = 0;  
+                mem_read = 0;  
+                mem_write = 0;  
+                alu_src = 0;  
+                reg_write = 0;  
       end  
       else begin  
       case(opcode)   
-      3'b000: begin // add  
-                reg_dst = 2'b01;  
-                mem_to_reg = 2'b00;  
+      6'b000000: begin // ADD, ADDU, SUB, SUBU, SLT, SLTU, AND, OR, NOR, SLL, SLLV, SRL, SRLV, SRA
+                reg_dst = 1;  
+                mem_to_reg = 0;  
                 alu_op = 2'b00;  
-                jump = 1'b0;  
-                branch = 1'b0;  
-                mem_read = 1'b0;  
-                mem_write = 1'b0;  
-                alu_src = 1'b0;  
-                reg_write = 1'b1;  
+                jump = 0;  
+                branch = 0;  
+                mem_read_write = 0;   
+                alu_src = 0;  
+                reg_write = 1;   
                 end  
-      3'b001: begin // sli  
-                reg_dst = 2'b00;  
-                mem_to_reg = 2'b00;  
-                alu_op = 2'b10;  
-                jump = 1'b0;  
-                branch = 1'b0;  
-                mem_read = 1'b0;  
-                mem_write = 1'b0;  
-                alu_src = 1'b1;  
-                reg_write = 1'b1;  
-                end  
-      3'b010: begin // j  
-                reg_dst = 2'b00;  
-                mem_to_reg = 2'b00;  
-                alu_op = 2'b00;  
-                jump = 1'b1;  
-                branch = 1'b0;  
-                mem_read = 1'b0;  
-                mem_write = 1'b0;  
-                alu_src = 1'b0;  
-                reg_write = 1'b0;  
-                end  
-      3'b011: begin // jal  
-                reg_dst = 2'b10;  
-                mem_to_reg = 2'b10;  
-                alu_op = 2'b00;  
-                jump = 1'b1;  
-                branch = 1'b0;  
-                mem_read = 1'b0;  
-                mem_write = 1'b0;  
-                alu_src = 1'b0;  
-                reg_write = 1'b1;  
-                end  
-      3'b100: begin // lw  
-                reg_dst = 2'b00;  
-                mem_to_reg = 2'b01;  
-                alu_op = 2'b11;  
-                jump = 1'b0;  
-                branch = 1'b0;  
-                mem_read = 1'b1;  
-                mem_write = 1'b0;  
-                alu_src = 1'b1;  
-                reg_write = 1'b1;  
-                end  
-      3'b101: begin // sw  
-                reg_dst = 2'b00;  
-                mem_to_reg = 2'b00;  
-                alu_op = 2'b11;  
-                jump = 1'b0;  
-                branch = 1'b0;  
-                mem_read = 1'b0;  
-                mem_write = 1'b1;  
-                alu_src = 1'b1;  
-                reg_write = 1'b0;  
-                end  
-      3'b110: begin // beq  
-                reg_dst = 2'b00;  
-                mem_to_reg = 2'b00;  
+      6'b011100: begin // CLO, CLZ
+                reg_dst = 1;  
+                mem_to_reg = 0;  
                 alu_op = 2'b01;  
-                jump = 1'b0;  
-                branch = 1'b1;  
-                mem_read = 1'b0;  
-                mem_write = 1'b0;  
-                alu_src = 1'b0;  
-                reg_write = 1'b0;  
+                jump = 0;  
+                branch = 0;  
+                mem_read_write = 0;   
+                alu_src = 0;  
+                reg_write = 1;  
+                end  
+      6'b000010: begin // JMP 
+                reg_dst = 0;  
+                mem_to_reg = 0;  
+                alu_op = 2'b00;  
+                jump = 1;  
+                branch = 0;  
+                mem_read_write = 0;   
+                alu_src = 0;  
+                reg_write = 0;  
+                end 
+
+      6'b101011: begin // SW 
+                reg_dst = 0;  
+                mem_to_reg = 1;  
+                alu_op = 2'b00;  
+                jump = 1;  
+                branch = 0;  
+                mem_read_write = 1;   
+                alu_src = 1;  
+                reg_write = 0;  
+                end
+
+      6'b000010: begin // JMP 
+                reg_dst = 0;  
+                mem_to_reg = 0;  
+                alu_op = 2'b00;  
+                jump = 1;  
+                branch = 0;  
+                mem_read_write = 0;   
+                alu_src = 0;  
+                reg_write = 0;  
                 end  
       default: begin  
                 reg_dst = 2'b01;  
